04d7b5d12624c7918bb58e64b3abda495dcd2121
harbar20 <harbar2021@gmail.com>
2022-11-01 20:23:08 -0400

    implemented getopt in server, need to test

--- qrcodes/server.c
+++ qrcodes/server.c
@@ -1,63 +1,114 @@
-#include <stdio.h>          /* for printf() and fprintf() */
+#include <stdio.h>		/* for printf() and fprintf() */
 #include <sys/socket.h> /* for socket(), bind(), and connect() */
-#include <arpa/inet.h>   /* for sockaddr_in and inet_ntoa() */
-#include <stdlib.h>       /* for atoi() and exit() */
-#include <string.h>       /* for memset() */
-#include <unistd.h>      /* for close() */
+#include <arpa/inet.h>	/* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>		/* for atoi() and exit() */
+#include <string.h>		/* for memset() */
+#include <unistd.h>		/* for close() */
+#include <getopt.h>		/* for getopt() */
 
 // #define MAXPENDING 5             /* Maximum outstanding connection requests */
-void DieWithError(char *errorMessage) {
+void DieWithError(char *errorMessage)
+{
 	printf("%s", errorMessage);
-};											/* Error handling function */
-void HandleTCPClient(int clntSocket) {
-	//TODO: QR Code decoding here
-};       /* TCP client handling function */
+}; /* Error handling function */
+void commandRunError(char *argv[])
+{
+	fprintf(stderr, "Usage:  %s -p <PORT between 2000-3000> -r <number requests> <number seconds> -m <Max Users> -t <Timeout in Seconds>\n", argv[0]);
+	exit(1);
+}
+void HandleTCPClient(int clntSocket){
+	// TODO: QR Code decoding here
+}; /* TCP client handling function */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	int servSock;                                   /*Socket descriptor for server */
-	int clntSock;                                   /* Socket descriptor for client */
+	int servSock;					 /*Socket descriptor for server */
+	int clntSock;					 /* Socket descriptor for client */
 	struct sockaddr_in echoServAddr; /* Local address */
 	struct sockaddr_in echoClntAddr; /* Client address */
-	unsigned short echoServPort;        /* Server port */
-	unsigned int clntLen;                     /* Length of client address data structure */
+	int option;
+	unsigned short PORT = 2012;				 /* Server port */
+	unsigned short RATE_NUMBER_REQUESTS = 3; /* Request limit for rate limit */
+	unsigned short RATE_NUMBER_SECONDS = 60; /* Time limit for rate limit */
+	unsigned short MAX_USERS = 3;
+	unsigned short TIME_OUT = 80;
 
-	if (argc != 2)     /* Test for correct number of arguments */
+	unsigned int clntLen; /* Length of client address data structure */
+
+	if (argc < 1 || argc > 10) /* Test for correct number of arguments */
 	{
-		fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
-		exit(1);
+		commandRunError(argv);
 	}
-	echoServPort = atoi(argv[1]);        /* First arg:  local port */
+
+	while ((option = getopt(argc, argv, ":p:r:m:t:")) != -1) /* Get the arguments */
+	{
+		switch (option)
+		{
+		case 'p':
+			if (!optarg)
+			{
+				commandRunError(argv);
+			}
+			PORT = optarg;
+		case 'r':
+			if (!optarg)
+			{
+				commandRunError(argv);
+			}
+			RATE_NUMBER_REQUESTS = optarg;
+			if (optind < argc && *argv[optind] != '-')
+			{
+				RATE_NUMBER_SECONDS = argv[optind];
+				optind++;
+			}
+			else
+			{
+				commandRunError(argv);
+			}
+		case 'm':
+			if (!optarg)
+			{
+				commandRunError(argv);
+			}
+			MAX_USERS = optarg;
+		case 't':
+			if (!optarg)
+			{
+				commandRunError(argv);
+			}
+			TIME_OUT = optarg;
+		}
+	}
+
 	/* Create socket for incoming connections */
-	if ((servSock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		printf("error");
-		// DieWithError("socket() failed");
+	DieWithError("socket() failed");
 	/* Construct local address structure */
-	memset(&echoServAddr, 0, sizeof(echoServAddr));         /* Zero out structure */
-	echoServAddr.sin_family         = AF_INET;                      /* Internet address family */
+	memset(&echoServAddr, 0, sizeof(echoServAddr));	  /* Zero out structure */
+	echoServAddr.sin_family = AF_INET;				  /* Internet address family */
 	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-	echoServAddr.sin_port             = htons(echoServPort);     /* Local port */
+	echoServAddr.sin_port = htons(PORT);			  /* Local PORT */
 
 	/* Bind to the local address */
-	if (bind (servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
-		printf("error");
-		// DieWithError("bind() failed");
+	if (bind(servSock, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0)
+		DieWithError("bind() failed");
 
 	/* Mark the socket so it will listen for incoming connections */
-	if (listen (servSock, MAXPENDING) < 0)
-		printf("error");
-		// DieWithError("listen() failed");
+	if (listen(servSock, MAXPENDING) < 0)
+		DieWithError("listen() failed");
+
 	for (;;) /* Run forever */
 	{
 		/* Set the size of the in-out parameter */
-		clntLen = sizeof(echoClntAddr);        /* Wait for a client to connect */
-		if ((clntSock = accept (servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
-			printf("error");
-			// DieWithError("accept() failed");
+		clntLen = sizeof(echoClntAddr); /* Wait for a client to connect */
+		if ((clntSock = accept(servSock, (struct sockaddr *)&echoClntAddr, &clntLen)) < 0)
+			DieWithError("accept() failed");
 
 		/* clntSock is connected to a client! */
 		printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
-		//HandleTCPClient(clntSock);
+		HandleTCPClient(clntSock);
 	}
 	/* NOT REACHED */
 }
